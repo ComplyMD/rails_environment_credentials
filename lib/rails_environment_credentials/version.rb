412fa195178dff2142db78545aa5b093fc762f13
Taylor Yelverton <taylor.yelverton@nuance.com>
2022-08-23 15:08:13 -0500

    Initial setup

---
+++ lib/rails_environment_credentials/version.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module RailsEnvironmentCredentials
+
+  module Version
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 1
+
+  end
+
+  VERSION = [
+    Version::MAJOR,
+    Version::MINOR,
+    Version::PATCH,
+  ].join('.').freeze
+
+end
