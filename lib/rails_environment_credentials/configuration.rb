3db3ab2d3fc3da0661e766d162b673e5526683ea
Taylor Yelverton <taylor.yelverton@nuance.com>
2022-08-29 16:34:31 -0500

    Better configuration.

--- lib/rails_environment_credentials/configuration.rb
+++ lib/rails_environment_credentials/configuration.rb
@@ -1,6 +1,36 @@
 # frozen_string_literal: true
 
 module RailsEnvironmentCredentials
+  class CredentialsConfig < ActiveSupport::OrderedOptions
+
+    def environment
+      super || default_credentials_environment
+    end
+
+    def content_path
+      super || default_credentials_content_path
+    end
+
+    def key_path
+      super || default_credentials_key_path
+    end
+
+    private
+
+      def default_credentials_environment
+        ENV.fetch('RAILS_CREDENTIALS_ENV') { Rails.env }
+      end
+
+      def default_credentials_content_path
+        Rails.root.join('config', 'credentials', "#{environment}.yml.enc")
+      end
+
+      def default_credentials_key_path
+        Rails.root.join('config', 'credentials', "#{environment}.key")
+      end
+
+  end
+
   module Configuration
     extend ActiveSupport::Concern
 
@@ -10,11 +40,9 @@ module RailsEnvironmentCredentials
 
     def initialize(*)
       super
-      @credentials = ActiveSupport::OrderedOptions.new
+      @credentials = CredentialsConfig.new
       @credentials.merge! credentials_config
-      @credentials.environment ||= default_credentials_environment
-      @credentials.content_path ||= default_credentials_content_path
-      @credentials.key_path ||= default_credentials_key_path
+      
     end
 
     private
@@ -22,18 +50,6 @@ module RailsEnvironmentCredentials
       def credentials_config
         path = root.join('config/credentials.yml')
         @credentials_config ||= (path.exist? ? YAML.safe_load(path.read) : {}).symbolize_keys
-      end
-
-      def default_credentials_environment
-        ENV.fetch('RAILS_CREDENTIALS_ENV') { Rails.env }
-      end
-
-      def default_credentials_content_path
-        root.join('config', 'credentials', "#{credentials.environment}.yml.enc")
-      end
-
-      def default_credentials_key_path
-        root.join('config', 'credentials', "#{credentials.environment}.key")
       end
 
   end
